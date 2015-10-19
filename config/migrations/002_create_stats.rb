jakubkriz/about_elastic_2014

[ADD] Init commit

config/migrations/002_create_stats.rb
@@ -1,0 +1,16 @@
+# encoding : utf-8
+
+Sequel.migration do
+  up do
+    create_table(:Stats) do
+      primary_key :id
+      foreign_key :link_id, :Stats, key: :id
+      String :likes_count
+      String :shares_count
+    end
+  end
+
+  down do
+    drop_table(:Stats)
+  end
+end

jerggi/sinatra-simple-api

config/migrations/002_create_stats.rb
@@ -1,0 +1,16 @@
+# encoding : utf-8
+
+Sequel.migration do
+  up do
+    create_table(:Stats) do
+      primary_key :id
+      foreign_key :link_id, :Stats, key: :id
+      String :like_count
+      String :share_count
+    end
+  end
+
+  down do
+    drop_table(:Stats)
+  end
+end
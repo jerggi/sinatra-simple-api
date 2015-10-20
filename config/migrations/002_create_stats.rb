b42f72da3ea4b4ab04f06e94eb656910b629b403
Tomáš Hrabal <fil.hrabal@gmail.com>
2015-10-20 12:50:06 +0200

    [FIX] Mistake with migration

--- config/migrations/002_create_stats.rb
+++ config/migrations/002_create_stats.rb
@@ -4,7 +4,7 @@ Sequel.migration do
   up do
     create_table(:Stats) do
       primary_key :id
-      foreign_key :link_id, :Stats, key: :id
+      foreign_key :link_id, :Links, key: :id
       String :like_count
       String :share_count
     end

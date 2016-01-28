d1c941332acc407a7276fe95cf9f2eb650885b45
jerggi <jerggi@gmail.com>
2016-01-28 08:32:15 +0100

    code refactoring to pass rubocop

--- lib/app.rb
+++ lib/app.rb
@@ -17,8 +17,12 @@ class FacebookStats < Sinatra::Base
   end
 
   helpers do
-    def get_all_links
+    def all_links
       @links = DB[:Links].all
+    end
+
+    def all_links_and_stats
+      @links = DB[:Links].join(:Stats, :link_id => :id).all
     end
   end
 
@@ -28,20 +32,65 @@ class FacebookStats < Sinatra::Base
 
   # #GET - homepage - get all links and form for adding new link
   get '/' do
-    get_all_links
+    all_links
     slim :index
   end
 
   # #POST - add link
+  post '/links' do
+    if params[:url].length > 0
+      url_input = params[:url]
+      result = Facebook.get_result url_input
+      p_key = DB[:Links].insert(:url => url_input, :domain => result['host'])
+      DB[:Stats].insert(:link_id => p_key, :like_count => result['like_count'], :share_count => result['share_count'])
+      all_links_and_stats
+      slim :links
+    else
+      all_links
+      slim :index
+    end
+  end
 
   # #DELETE - delete link
+  delete '/link/:id' do |id|
+    DB[:Stats].filter(:link_id => id).delete
+    DB[:Links].filter(:id => id).delete
+    all_links
+    slim :index
+  end
 
   # #GET - get links statistics
+  get '/link/:id/stats' do |id|
+    @stats = DB[:Stats].filter(:link_id => id)
+    slim :stats
+  end
 
   # #GET - get form for edit link informations
+  get '/link/:id/edit' do |id|
+    @link = DB[:Links].first(:id => id)
+    slim :link_form
+  end
 
   # #PUT - edit link information
+  put '/link/:id' do |id|
+    DB[:Links].where('id = ?', id).update(:url => params[:url], :domain => 'domain')
+    result = Facebook.get_result params[:url]
+    lc = result['like_count']
+    sc = result['share_count']
+    DB[:Stats].where('link_id = ?', id).update(:like_count => lc, :share_count => sc)
+    all_links
+    slim :index
+  end
 
   # #GET - refresh all links statistics iwth download_stats
-
+  get '/refresh' do
+    DB[:Links].all.each do |link|
+      result = Facebook.get_result link[:url]
+      lc = result['like_count']
+      sc = result['share_count']
+      DB[:Stats].where('link_id = ?', link[:id]).update(:like_count => lc, :share_count => sc)
+    end
+    all_links_and_stats
+    slim :links
+  end
 end
--- lib/helpers/facebook.rb
+++ lib/helpers/facebook.rb
@@ -1,5 +1,5 @@
 #!/usr/bin/env ruby
-#encoding:utf-8
+# encoding:utf-8
 require 'net/http'
 require 'json'
 require 'uri'
@@ -7,6 +7,40 @@ require 'pp'
 
 module Facebook
   class << self
+    FACEBOOK_API_URL   = 'http://api.facebook.com/'
+    QUERY              = 'method/links.getStats?urls=#query#&format=json'
+    UNKNOWN            = 'neznámý'
 
+    def respond(url)
+      uri_server = URI('http://api.facebook.com/method/links.getStats?urls=' + url + '&format=json')
+      resp = Net::HTTP.get_response(uri_server)
+      resp.body
+    rescue URI::InvalidURIError
+      '[]'
+    end
+
+    def domain(url)
+      url = 'http://' + url if %r{^[a-z]+://}.match(url).nil?
+      URI(url).host
+    rescue URI::InvalidURIError
+      UNKNOWN
+    end
+
+    def get_result(url)
+      base_domain = like_count = share_count = UNKNOWN
+      resp = respond(url)
+      if resp.length > 2
+        resp_obj = JSON.parse(resp[1...-1])
+        like_count = resp_obj["like_count"]
+        share_count = resp_obj["share_count"]
+        base_domain = domain url
+      end
+      {
+        'host'                 => base_domain, # base domain,
+        'url'                  => url,
+        'like_count'           => like_count, # like_count
+        'share_count'          => share_count # share_count#
+      }
+    end
   end
 end
